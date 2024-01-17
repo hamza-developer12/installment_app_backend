3deab9df1b6a65c8afa096df50e008e12cfe2beb
Hamza Azam <hamzaazam411@gmail.com>
2024-01-17 17:00:14 +0500

    Modified fileUpload

--- utils/fileUpload.js
+++ utils/fileUpload.js
@@ -1,9 +1,9 @@
 const multer = require("multer");
 const path = require("path");
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads");
-  },
+  // destination: function (req, file, cb) {
+  //   cb(null, "uploads");
+  // },
   filename: function (req, file, cb) {
     const name = Date.now() + "-" + file.originalname;
     cb(null, name);
